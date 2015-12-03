0204ef86026fe1382d5be6c07c128739db96917d
Jared McFarland <jared.online@gmail.com>
2015-12-03 18:19:31 +0100

    toggles!

--- js/components/App.js
+++ js/components/App.js
@@ -3,23 +3,37 @@ import Relay from 'react-relay';
 
 import ItemTable from './ItemTable';
 
-class AppComponent extends React.Component {
+var AppComponent = React.createClass({
+  getInitialState() {
+    return {grid: false};
+  },
+  toggleGrid() {
+    this.setState({grid: !this.state.grid});
+  },
   render() {
+    var itemDisplay, gridToggleText;
+    if (this.state.grid) {
+        gridToggleText = "View as a list";
+        itemDisplay = <div />
+    } else {
+        gridToggleText = "View as a grid";
+        itemDisplay = <ItemTable key={this.props.me.id} user={this.props.me} />
+    }
     return (
       <div className="container">
         <div className="row">
             <div className="col-md-12">
-                <h1>Item list ({this.props.me.items.length})</h1>
+                <h1>Item list ({this.props.me.items.length}) <a onClick={this.toggleGrid}>{gridToggleText}</a></h1>
                 <p>{this.props.me.email}</p>
             </div>
         </div>
         <div className="row">
-            <ItemTable key={this.props.me.id} user={this.props.me} />
+            {itemDisplay}
         </div>
       </div>
     );
   }
-}
+})
 
 export default Relay.createContainer(AppComponent, {
   fragments: {
